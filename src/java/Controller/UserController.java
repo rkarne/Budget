f136b118ef583a6ed2483b37aea838c0e7d19c40
roja jayashree <roja4jayashree@gmail.com>
2017-04-04 17:01:58 -0400

    user login

---
+++ src/java/Controller/UserController.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Connection.DBUtils;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.bean.ApplicationScoped;
+import javax.inject.Named;
+import pojo.User;
+
+/**
+ *
+ * @author rkarne
+ */
+
+@Named
+@ApplicationScoped
+public class UserController {
+    private List<User> users;
+    private User userobj;
+    
+   public UserController(){
+        userobj = new User(-1, "", "", "");
+        getData();
+   }
+   
+   private void getData(){
+        try {
+            users = new ArrayList<>();
+            Connection con = DBUtils.getConnection();
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM userlogin");
+            while(rs.next()){
+                User us = new User(
+                 rs.getInt("id"),
+                 rs.getString("username"),
+                 rs.getString("userpass"),
+                 rs.getString("name")
+                );
+                users.add(us);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+   }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public User getUserobj() {
+        return userobj;
+    }
+   
+   
+}
--- src/java/pojo/User.java
+++ src/java/pojo/User.java
@@ -10,5 +10,47 @@ package pojo;
  * @author rkarne
  */
 public class User {
+    private int id;
+    private String userName;
+    private String userPassword;
+    private String name;
     
+    public User(int id, String usname, String uspass, String name){
+        this.id = id;
+        this.userName = usname;
+        this.userPassword = uspass;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
